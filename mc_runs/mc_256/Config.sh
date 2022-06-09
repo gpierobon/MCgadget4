04bc49a3967afe71c8d9a4e16d266932704e16fb
gpierobon <gpierobon@users.noreply.github.com>
2022-06-09 23:12:10 +1000

    Folders to build the code

---
+++ mc_runs/mc_256/Config.sh
@@ -0,0 +1,11 @@
+PERIODIC                                     # enables periodic boundary condistions
+NTYPES=2                                     # number of particle types 
+
+SELFGRAVITY
+
+PMGRID=256                                   # basic mesh size for TreePM calculations
+NSOFTCLASSES=2                               # number of different softening classes
+
+POSITIONS_IN_32BIT                           # if set, use 32-integers for positions  (default for single precision)
+IDS_32BIT                                    # selects 32-bit IDs for internal storage (default)
+POWERSPEC_ON_OUTPUT                          # computes a matter power spectrum when the code writes a snapshot output
